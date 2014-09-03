4e4edf9c271000aa4b61f2d26ccfa958bd1a2d6e
dhb317 <dhb317@lehigh.edu>
2014-09-03 00:42:12 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,35 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Dhruv Bajoria CSE-002
+//Homework 01 09/02/2014
+//Welcome Class Java Program
+//
+//  first compile the program
+//   javac WelcomeClass.Java
+//  run the program
+//   java WelcomeClass//
+//  
+//  deinfe the class
+public class WelcomeClass{
+
+//  add main method
+  public static void main(String[] args) {
+      
+//  print the statement with a box around it
+    System.out.println("     -----------    ");
+    System.out.println("     | WELCOME |    ");
+    System.out.println("     -----------    ");
+    System.out.println("    ^ ^ ^ ^ ^ ^ ^   ");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("|-D--H--B--3--1--7-|");
+    System.out.println("\\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("    v v v v v v v   ");
+    System.out.println("This summer I went back to my house");
+    System.out.println("which was in India. ");
+    
+    //test/testtest
+  } 
+}   
+    
+
+  
+
